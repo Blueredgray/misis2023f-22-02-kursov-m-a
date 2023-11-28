f0d6c4c31d138e22e1c4451d352a5fd9b473e0be
Kursov Mikhail <113849163+Blueredgray@users.noreply.github.com>
2023-11-28 23:01:34 +0300

    Add files via upload

--- prj.cw/hexobox_lib/hb_test.cpp
+++ prj.cw/hexobox_lib/hb_test.cpp
@@ -1,21 +1,29 @@
 #include "hexbox.h"
+#include <chrono>
+
+
+void test_hexbox(cint it) {
+    int time = 0;
+    for (int i = 0; i < it; i += 1) {
+        auto begin = std::chrono::steady_clock::now();
+        //std::cout << "\n";
+        int x = 100;
+        int y = 100;
+        Hexbox hb(x, y);
+        hex_args Hex1{ 5, 6 };
+        Hex1.hex_h = 3;
+        hb.hex_grid[2] = Hex1;
+        int n = 3;
+
+        auto end = std::chrono::steady_clock::now();
+        auto elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+
+        time += elapsed_ms.count();
+
+    }
+    std::cout << std::endl <<"Time avg : \t" << int(time / it);
+}
 
 int main() {
-    int x = 4;
-    int y = 4;
-    Hexbox hb(x, y);
-    hex_args Hex1{ 5, 6 };
-    Hex1.hex_h = 1;
-    std::cout << hb.count_st << std::endl;
-    //hb.hex_grid[3] = Hex1;
-    std::cout << hb.hex_grid[3].hex_h << std::endl;
-    //std::cout << "\t\t\t" << hb.hex_grid[3].type_mat;
-    /*int n = 10;
-    std::cout << "\t" << hb.hex_grid[n].st[0];
-    std::cout << "\t" << hb.hex_grid[n].st[1];
-    std::cout << "\t" << hb.hex_grid[n].st[2];
-    std::cout << "\t" << hb.hex_grid[n].st[3];
-    std::cout << "\t" << hb.hex_grid[n].st[4];
-    std::cout << "\t" << hb.hex_grid[n].st[5];*/
-    //hex_args Hex1 = hb.GetHex(0, 0);
+    test_hexbox(1000);
 }
